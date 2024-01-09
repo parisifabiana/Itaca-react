7e4dd15fa3f754e5070913777b3d7a29dc9f32ef
Fabiana Parisi <parisifabiana@gmail.com>
2024-01-09 21:42:45 +0100

    last changes

--- lez-3/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { App } from "../src/App.jsx";
-
-const appElement = document.getElementById("app");
-const root = ReactDOM.createRoot(appElement);
-root.render(<App title="This is an awesome React App" />);
